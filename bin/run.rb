dd22869149c5a7ca1bc8d73302ab5d9c8bc971af
github username <github email address>
2019-03-04 15:48:08 +0000

    realtime sound

--- bin/run.rb
+++ bin/run.rb
@@ -3,7 +3,8 @@ require 'pry'
 
 # class Sound < Gosu::Window
 #     def initialize
-#         @sound =Gosu::Sample.new("lib/sounds/Kits/01/Bass.wav")
+#         @s1 =Gosu::Sample.new("lib/sounds/Kits/01/Bass.wav")
+#         @s2 =Gosu::Sample.new("lib/sounds/Kits/01/BassStab.wav")
 #     end
 
 #     def play_sound
@@ -11,5 +12,52 @@ require 'pry'
 #     end
 # end
 
-pry
-puts "HELLO WORLD"
+# def button_a(id)
+#     case id
+#     when Gosu::KB_A
+#         @sound
+#     end
+    
+# end
+
+
+# class Play_sounds
+#     def initialize
+#         @s1 =Gosu::Sample.new("lib/sounds/Kits/01/Bass.wav")
+#         @s2 =Gosu::Sample.new("lib/sounds/Kits/01/BassStab.wav")
+#     end
+
+#     def play_s1
+#         @s1.play
+#     end
+
+#     def play_s2
+#         @s2.play
+#     end
+# end
+
+# class My_window < Gosu::Window
+#     def initialize
+#         super 640, 480
+#         self.caption = "Tutorial Game"
+
+#         @play_sounds = Play_sounds.new
+#     end
+
+#     def update
+#         if Gosu.button_down? Gosu::KB_A
+#             @play_sounds.play_s1
+#         end
+
+#         if Gosu.button_down? Gosu::KB_S
+#             @play_sounds.play_s2
+#         end
+#     end
+    
+#     def draw
+#     # ...
+#     end
+# end
+
+# My_window.new.show
+# puts "HELLO WORLD"
