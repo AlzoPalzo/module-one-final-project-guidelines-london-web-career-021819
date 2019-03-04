73fe123b1c23d50a999bc02349d29d4ef94868ad
github username <github email address>
2019-03-04 14:52:12 +0000

    Learnt some sound

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,15 @@
 require_relative '../config/environment'
+require 'pry'
 
+# class Sound < Gosu::Window
+#     def initialize
+#         @sound =Gosu::Sample.new("lib/sounds/Kits/01/Bass.wav")
+#     end
 
+#     def play_sound
+#         @sound.play
+#     end
+# end
 
+pry
 puts "HELLO WORLD"
